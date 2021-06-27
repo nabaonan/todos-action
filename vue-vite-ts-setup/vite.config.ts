6d415e365e5de591a76c2a92c83aa9b4cf3fbda0
nabaonan <nbn1209@126.com>
2021-06-27 19:16:03 +0800

    opt: load antd style ondemand

--- vue-vite-ts-setup/vite.config.ts
+++ vue-vite-ts-setup/vite.config.ts
@@ -14,114 +14,96 @@ import ViteComponents, {
 } from "vite-plugin-components";
 import { generateModifyVars } from "./build/style/generateModifyVars";
 // https://vitejs.dev/config/
-
-function getCompPath(
-  compName: string
-): {
-  dirName: string;
-  compName: string;
+interface IMatcher {
+  pattern: RegExp;
   styleDir: string;
-  importName?: string;
-  sideEffects?: ImportInfo;
-} {
-  const hasSubComp = [
-    "Menu",
-    "Layout",
-    "Form",
-    "Table",
-    "Modal",
-    "Radio",
-    "Button",
-    "Checkbox",
-    "List",
-    "Collapse",
-    "Descriptions",
-    "Tabs",
-    "Mentions",
-    "Select",
-    "Anchor",
-    "Typography",
-    // "TreeSelect",
-  ]; //包含子组件的组件
-  const keepSelf = [
-    "MenuItem",
-    "SubMenu",
-    "FormItem",
-    "RadioButton",
-    "CollapsePanel",
-    "TabPane",
-    "AnchorLink",
-  ]; //保留原子组件名称
-  const keepFather = [
-    "LayoutHeader",
-    "LayoutContent",
-    "LayoutFooter",
-    "DescriptionsItem",
-  ]; //需要使用父组件名称的子组件  LayoutFooter->''  之所以转成空是因为最后拼接的结果是dirName+compName，避免重复
-  const rootName = hasSubComp.find((name: string) => compName.startsWith(name));
-  const usePrevLevelName = ["ListItemMeta"]; //使用当前组件的上一级名称  ListItemMeta->Item
-  const getPrevLevelName = () => {
-    const split = kebabCase(compName).split("-");
-    return pascalCase(split[split.length - 2]);
-  };
-
-  const fatherAlias = {
-    TabPane: "vc-tabs/src",
-    MentionsOption: "vc-mentions/src",
-    SelectOption: "vc-select",
-    TreeSelectNode: "vc-tree-select/src",
-  };
-
-  const compAlias = {
-    TreeSelectNode: "SelectNode",
-  };
-
-  const styleMap = {
-    TabPane: "tabs",
-    MentionsOption: "mentions",
-    SelectOption: "select",
-    TreeSelectNode: "tree-select",
-  };
-  // const importNameMap = {
-  //   LayoutContent: "Content",
-  //   LayoutHeader: "Header",
-  //   LayoutFooter: "Footer",
-  // };
-
-  let dirName = rootName?.toLowerCase() ?? kebabCase(compName);
-
-  if (fatherAlias[compName]) {
-    dirName = fatherAlias[compName];
-  }
-
-  let compNameStr = "";
-  if (keepSelf.includes(compName)) {
-    compNameStr = compName;
-  } else if (keepFather.includes(compName)) {
-    compNameStr = "";
-  } else if (usePrevLevelName.includes(compName)) {
-    compNameStr = getPrevLevelName();
-  } else if (rootName) {
-    compNameStr = compName.replace(rootName, "");
-  }
-  const compRequired = {
-    TypographyTitle: "ant-design-vue/es/" + dirName + "/Base",
-    TypographyText: "ant-design-vue/es/" + dirName + "/Base",
-  };
-
-  return {
-    // importName: importNameMap[compName],
-    dirName: fatherAlias[compName] ?? dirName,
-    styleDir: `${styleMap[compName] ?? dirName}`,
-    compName: compAlias[compName] ?? compNameStr,
-    sideEffects: compRequired[compName]
-      ? {
-          path: compRequired[compName],
-        }
-      : undefined,
-  };
 }
-
+const matchComponents: IMatcher[] = [
+  {
+    pattern: /^Menu/,
+    styleDir: "menu",
+  },
+  {
+    pattern: /^Layout/,
+    styleDir: "menu",
+  },
+  {
+    pattern: /^Form/,
+    styleDir: "form",
+  },
+  {
+    pattern: /^Table/,
+    styleDir: "table",
+  },
+  {
+    pattern: /^Radio/,
+    styleDir: "radio",
+  },
+  {
+    pattern: /^Dropdown/,
+    styleDir: "dropdown",
+  },
+  {
+    pattern: /^Button/,
+    styleDir: "button",
+  },
+  {
+    pattern: /^Checkbox/,
+    styleDir: "checkbox",
+  },
+  {
+    pattern: /^List/,
+    styleDir: "list",
+  },
+  {
+    pattern: /^Collapse/,
+    styleDir: "collapse",
+  },
+  {
+    pattern: /^Descriptions/,
+    styleDir: "descriptions",
+  },
+  {
+    pattern: /^Tabs/,
+    styleDir: "tabs",
+  },
+  {
+    pattern: /^Mentions/,
+    styleDir: "mentions",
+  },
+  {
+    pattern: /^Select/,
+    styleDir: "select",
+  },
+  {
+    pattern: /^Mentions/,
+    styleDir: "mentions",
+  },
+  {
+    pattern: /^Anchor/,
+    styleDir: "anchor",
+  },
+  {
+    pattern: /^Typography/,
+    styleDir: "typography",
+  },
+  {
+    pattern: /^TreeSelect/,
+    styleDir: "tree-select",
+  },
+  {
+    pattern: /^Tree/,
+    styleDir: "tree",
+  },
+  {
+    pattern: /^Step/,
+    styleDir: "steps",
+  },
+  {
+    pattern: /^RangePicker|^WeekPicker|^MonthPicker/,
+    styleDir: "date-picker",
+  },
+];
 export default defineConfig({
   plugins: [
     vue(),
@@ -140,13 +122,27 @@ export default defineConfig({
               sideEffects: `element-plus/lib/theme-chalk/${partialName}.css`,
             };
           } else if (name.match(/^A[A-Z]/)) {
-            //ant-design-vue
+            //Ant-Design-Vue
 
             const importName = name.slice(1);
+            let styleDir;
+            const total = matchComponents.length;
+            for (let i = 0; i < total; i++) {
+              const matcher = matchComponents[i];
+              if (importName.match(matcher.pattern)) {
+                styleDir = matcher.styleDir;
+                break;
+              }
+            }
+
+            if (!styleDir) {
+              styleDir = kebabCase(importName);
+            }
+
             return {
               importName: importName,
               path: `ant-design-vue/es`,
-              sideEffects: "ant-design-vue/es/style",
+              sideEffects: `ant-design-vue/es/${styleDir}/style`,
             };
           }
           return null;
@@ -154,27 +150,6 @@ export default defineConfig({
       ],
       globalComponentsDeclaration: true,
     }),
-    // usePluginImport({
-    //   libraryName: "ant-design-vue",
-    //   libraryDirectory: "es",
-    //   style: "css",
-    // }),
-
-    // styleImport({
-    //   libs: [
-    //     {
-    //       libraryName: "element-plus",
-    //       esModule: true,
-    //       ensureStyleFile: true,
-    //       resolveStyle: name => {
-    //         return `element-plus/lib/theme-chalk/${name}.css`;
-    //       },
-    //       resolveComponent: name => {
-    //         return `element-plus/lib/${name}`;
-    //       },
-    //     },
-    //   ],
-    // }),
   ],
   resolve: {
     alias: [
